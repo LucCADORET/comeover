ceb5d666849fe749059bb958bb7b05963df53d1b
Luc <luc.cadoret@gmail.com>
2019-12-01 11:13:35 +0100

    Trying to enforce https

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+var redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(path.join(__dirname, 'dist')));
+
+// Don't redirect if the hostname is `localhost:port` or the route is `/insecure`
+app.use(redirectToHTTPS([/localhost:(\d{4})/], [/\/insecure/], 301));
 
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
