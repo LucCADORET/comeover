13a29b950d09deb2f6f06302724369311af7c813
Luc <luc.cadoret@gmail.com>
2019-12-04 11:20:17 +0100

    Changing the name of the app in the configuration files + better config for the build staging/prod

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('come-over-client app is running!');
+    expect(page.getTitleText()).toEqual('comeover-client app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'come-over-client'`, () => {
+  it(`should have as title 'comeover-client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('come-over-client');
+    expect(app.title).toEqual('comeover-client');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('come-over-client app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('comeover-client app is running!');
   });
 });
