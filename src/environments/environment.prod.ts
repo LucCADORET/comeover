0815651812f861f84e50a5cdddc099419832f591
Luc Cadoret <luc.cadoret@gmail.com>
2020-06-09 11:49:33 +0200

    Adding production and stating env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  targetDuration: 10, // Target duration of the live chunks when recording a media stream
+  recordingMimeType: 'video/webm; codecs=vp9',
 };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,3 +1,5 @@
 export const environment = {
-    production: false
+    production: false,
+    targetDuration: 10, // Target duration of the live chunks when recording a media stream
+    recordingMimeType: 'video/webm; codecs=vp9',
 };
