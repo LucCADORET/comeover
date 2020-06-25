9942877a849205bea1a0f7179ba70cbab10f81c8
Luc <luc.cadoret@gmail.com>
2020-06-25 15:09:09 +0200

    Changing lib for ssl redirect

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-var redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
+var sslRedirect = require('heroku-ssl-redirect');
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Don't redirect if the hostname is `localhost:port` or the route is `/insecure`
-app.use(redirectToHTTPS([/localhost:(\d{4})/], [/\/insecure/], 301));
+app.use(sslRedirect());
 
 // send the user the ownership google file
 app.get('/robots.txt', (req, res) => {
